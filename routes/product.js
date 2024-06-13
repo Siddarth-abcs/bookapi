afebb80fd1d3777879a4fd544175cfee0262293f
Developer Siddarth <142804820+Siddarth-abcs@users.noreply.github.com>
2024-06-13 22:11:58 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -10,6 +10,6 @@ router
   .post("/upload", upload.single("file"), controller.uploadfile)
   .get("/products", controller.allproducts)
   .delete("/delete/:id", controller.deleteProduct)
-  .put("/update", controller.updateProduct);
+  .put("/update/:id" , controller.updateProduct);
 
 exports.routes = router;
