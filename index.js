78eacc5e89965f00734dcfc99f5b6af54be48fc5
Developer Siddarth <142804820+Siddarth-abcs@users.noreply.github.com>
2024-06-18 20:50:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,9 +42,13 @@ app.use(
     store: mongostore,
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 7 * 24  * 60 * 60 * 1000 },
+    cookie: { 
+      maxAge: 7 * 24 * 60 * 60 * 1000, // Example: 7 days
+      secure: true,  // Ensures the cookie is only sent over HTTPS
+    },
   })
 );
+
 
 app.use("/", productrouter.routes);
 
