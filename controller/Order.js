f176c180504b482e7d233e7d4c3fd732247cff31
Developer Siddarth <142804820+Siddarth-abcs@users.noreply.github.com>
2024-06-26 18:43:14 +0530

    deleteorder

--- controller/Order.js
+++ controller/Order.js
@@ -53,9 +53,13 @@ exports.Order = async (req, res) => {
 
 exports.deleteOrder = async (req, res) => {
   try {
-    const Order = await Order.findByIdAndDelete(req.params.id);
+    const order = await Order.findByIdAndDelete(req.params.id);
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
     res.status(200).json({ message: "Order deleted successfully" });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
+
